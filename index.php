9c6aa996225adcf555973a98ccc57213fe4482ee
Rajveer Solanki <raj120620@gmail.com>
2022-11-03 12:37:23 +0530

    Added company and position in index.php

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 <h1>Rajveer's Profile Page</h1>
 <a href="CV.html" target="_blank"><img src="profile.JPG" alt="Profile Picture"></a>
 <h4>Full Name : Rajveer Solanki</h4>
+<h3>Groww India Pvt Limited</h3>
+<h3>Software Development Engineer</h3>
 <h4>Email : rajveersolanki560@gmail.com</h4>
 <h4>Mobile : +91 7974014026</h4>
 <h4>Address : 32, Shivaji Marg, Alirajpur</h4>
